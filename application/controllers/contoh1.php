b564c1ece8f5b18c38d818022ded5704bb1b0189
raft86 <rgmilan86@gmail.com>
2024-03-27 16:55:39 +0700

    Mata Kuliah

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -5,6 +5,5 @@ class contoh1 extends CI_Controller
     {
         echo "<h1>Perkenalkan</h1>";
         echo "Nama Saya Rengginang, saya tinggal di dalam toples";
-        echo "<h2>TAPI BOONG WKWKWKW </h2>";
     }
 }
