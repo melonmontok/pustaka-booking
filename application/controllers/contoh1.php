c6537d40c5381f5b2d80e4178a138c47c26e10d2
raft86 <rgmilan86@gmail.com>
2024-03-27 14:25:16 +0700

    Mulai pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Rengginang, saya tinggal di dalam toples";
+        echo "<h2>TAPI BOONG WKWKWKW </h2>";
+    }
+}
