47b5207b83e7e8944fdb5ddbfb1453f853ee7a97
raft86 <rgmilan86@gmail.com>
2024-04-05 16:40:16 +0700

    web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Gabriel</li>
+        <li>Nama Belakang : Angelica</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Muria Tanjakan</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="Square">
+        <li>Basket</li>
+        <li>Dodgeball</li>
+    </ul>
+    </section>
