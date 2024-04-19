25bea604a0a3357e8af47befdee5ef4ce2867346
raft86 <rgmilan86@gmail.com>
2024-04-19 16:28:55 +0700

    validasi

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -8,6 +8,7 @@
 <body>
     <center>
         <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+        <?php echo validation_errors()?>
             <table>
                 <tr>
                     <th colspan="3">
